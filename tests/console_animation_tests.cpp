ca4fb82f65aa9f05a62278c39a47bcbdb8f3eacd
Marius Feilhauer <mail@mariusfeilhauer.de>
2018-09-15 21:23:01 +0200

    Added additional test for the console_animation

--- tests/console_animation_tests.cpp
+++ tests/console_animation_tests.cpp
@@ -14,11 +14,37 @@ TEST(animation, default_usage) {
 
 	auto l1 = std::string("###############");
 	auto l2 = std::string("# Hello World #");
-	auto l3 = l1;
-	auto frame = { l1, l2, l3 };
+	auto frame = { l1, l2, l1 };
 
 	ca.set_frame(frame);
 
-	auto expected = l1 + "\n" + l2 + "\n" + l3 + "\n";
+	auto expected = l1 + "\n" + l2 + "\n" + l1 + "\n";
 	ASSERT_BUFFER(expected.c_str(), s);
+}
+
+TEST(animation, simple_animation) {
+	std::stringstream s;
+	console_animation ca(s);
+
+	auto l1 = std::string("###############");
+	auto l2 = std::string("# Hello World #");
+	auto frame1 = { l1, l2, l1 };
+	auto frame2 = { l2, l1 };
+	auto frame3 = { l1 };
+
+	ca.set_frame(frame1);
+	ca.set_frame(frame2);
+	ca.set_frame(frame3);
+
+	auto expected = l1 + "\n";;
+	ASSERT_BUFFER(expected.c_str(), s);
+}
+
+TEST(animation, empty_frame) {
+	std::stringstream s;
+	console_animation ca(s);
+
+	auto frame = { std::string() };
+	ca.set_frame(frame);
+	ASSERT_BUFFER("\n", s);
 }
